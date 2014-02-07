641f200ab6c9e2f338b488c246239efeb9e43dbb
Pascal Poizat <pascal.poizat@lip6.fr>
2014-02-07 16:19:17 +0100

    integration of old code (was not in the repository)

--- src/cif/CifChoreographySpecification.java
+++ src/cif/CifChoreographySpecification.java
@@ -6,12 +6,15 @@ import java.util.HashMap;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.List;
+import java.util.ArrayList;
 
 import models.choreography.cif.CifModel;
 import models.base.IllegalResourceException;
 
 /**
  * Created by pascalpoizat on 05/02/2014.
+ * translation in Java and refactoring of Python code by the VerChor team
  */
 public class CifChoreographySpecification extends ChoreographySpecification {
 
@@ -26,6 +29,8 @@ public class CifChoreographySpecification extends ChoreographySpecification {
     private static final String synchronous_composition_suffix = "_compo_sync";
     private static final String synchronizability_equivalence = "strong comparison using bfs";
     private static final String realizability_equivalence = "strong comparison using bfs";
+    private static final String reduction_smart = "smart branching";
+    private static final String reduction_no_smart = "root leaf branching";
     private static final String equivalence_checker = "bisimulator";
     private static final String svl_suffix = ".svl";
     private static final String lnt_suffix = ".lnt";
@@ -161,6 +166,70 @@ public class CifChoreographySpecification extends ChoreographySpecification {
     @Override
     public void about() {
         System.out.println(NAME + " " + VERSION);
+    }
+
+    // OLD
+
+    private String computeSvlDefinitions(String name, List<String> alphabet, List<String> peers, String reduction, boolean generatePeers) {
+        String script = "";
+        //
+        script += "% CAESAR_OPEN_OPTIONS=\"-silent -warning\"\n% CAESAR_OPTIONS=\"-more cat\"\n\n";
+        script += "% DEFAULT_PROCESS_FILE=" + name + ".lnt\n\n";
+        //
+        script += "\"" + name + "_bpmnlts_min.bcg\" = safety reduction of tau*.a reduction of branching reduction of \"MAIN [" + generateSvlAlphabet(alphabet, false, false) + "]\";\n\n";
+        //
+        script += "\"" + name + "_compo_sync.bcg\" = " + reduction + " reduction of\n" + generateSvlSyncRedCompositional(alphabet, peers);
+        //
+        script += "\"" + name + "_compo_sync_min.bcg\"= weak trace reduction of safety reduction of tau*.a reduction of branching reduction of \"" + name + "_compo_sync.bcg\";\n\n";
+        //
+        script += "\"" + name + "_acompo.bcg\" = " + reduction + " reduction of\n" + generateSvlAsyncRedCompositional(alphabet, peers, true);
+        //
+        script += "\"" + name + "_acompo_min.bcg\"= safety reduction of tau*.a reduction of branching reduction of \"" + name + "_acompo.bcg\";\n\n";
+        //
+        if (generatePeers) {
+            for (String peer : peers) {
+                script += "\"" + name + "_peer_" + peer + ".bcg\" = safety reduction of tau*.a reduction of \"peer_" + peer + " [" + generateSvlAlphabet(alphabet, false, false) + "]\";\n\n";
+                script += "\"" + name + "_apeer_" + peer + ".bcg\" = safety reduction of tau*.a reduction of \"apeer_" + peer + " [";
+                script += generateSvlAlphabet(computeDirAlphabetforPeer(peer, computePeerAlphabetForPeer(peer, alphabet)), false, false);
+                script += "]\";\n\n";
+            }
+        }
+        //
+        return script;
+    }
+
+    // computes ...
+    private List<String> computeDirAlphabetforPeer(String peer, List<String> alphabet) {
+        return new ArrayList<String>(); // TODO
+    }
+
+    // computes ...
+    private List<String> computePeerAlphabetForPeer(String peer, List<String> alphabet) {
+        return new ArrayList<String>(); // TODO
+    }
+
+    // computes the alphabet of a choreography
+    private List<String> computeAlphabet() {
+        return new ArrayList<String>(); // TODO
+    }
+
+    // computes the list of peers of a choreography
+    private List<String> computePeers() {
+        return new ArrayList<String>(); // TODO
+    }
+
+    // generates a string for an alphabet
+    private String generateSvlAlphabet(List<String> alphabet, boolean withAny, boolean withSynchronizingMessage) {
+        return "* the alphabet *"; // TODO
+    }
+
+    // generates ...
+    private String generateSvlSyncRedCompositional(List<String> alphabet, List<String> peers) {
+        return "* a sync red *"; // TODO
+    }    // generates ...
+
+    private String generateSvlAsyncRedCompositional(List<String> alphabet, List<String> peers, boolean withHiding) {
+        return "* an async red *"; // TODO
     }
 
 
